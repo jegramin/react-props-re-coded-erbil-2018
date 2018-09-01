0124fe8e82b38c98e20f6275df876f64821e739c
Mohammed Ameen <jegroamin@gmail.com>
2018-09-01 11:55:11 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MovieCard extends React.Component {
+export default class MovieCard extends React.Component {
   render() {
     return (
       <div className="movie-card">
